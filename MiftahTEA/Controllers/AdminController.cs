c2fc63f76b0288bd839282bebe2774a5656c5300
ferit.ekta <renault.laguna.973@gmail.com>
2026-02-18 13:26:25 +0300

    merhaba arkadaşlar

--- MiftahTEA/Controllers/AdminController.cs
+++ MiftahTEA/Controllers/AdminController.cs
@@ -11,6 +11,7 @@ namespace MiftahTEA.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    //projeyi bozdum
     public class AdminController : ControllerBase
     {
         private readonly IApplicationDbContext _context;
